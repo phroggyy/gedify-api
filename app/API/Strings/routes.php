b545ab5dc078c4d99bfdb0c71f6226ebbeba9c30
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-22 02:38:57 +0200

    Save a good 7 bytes

--- app/API/Strings/routes.php
+++ app/API/Strings/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-$router->group(['prefix' => 'v1'], function () use ($router) {
+$router->group(['prefix' => 'v1'], function ($router) {
     $router->post('reverse', ['uses' => 'ApiController@reverse', 'as' => 'strings.reverse']);
 });
